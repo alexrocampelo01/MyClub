7bf2bae5d9f4adea65c250ee327154a8d0992da5
alexrocampelo01 <alejandrorodriguezcampelo@gmail.com>
2024-06-24 00:15:24 +0200

    cambio de credenciales base de datos servidor

--- Codigo/php/conet.php
+++ Codigo/php/conet.php
@@ -4,8 +4,8 @@
 
     private $host = "localhost";
     private $db = "myclub";
-    private $user = "myclub";
-    private $pass = "myclub";
+    private $user = "myclub1";
+    private $pass = "myclub1";
     public function __construct() {
         try {
             parent::__construct($this->host, $this->user, $this->pass, $this->db);
